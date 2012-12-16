c19e4609b1db33a705d43180ea2a4d6350d20821
Moxley Stratton <moxley@theclymb.com>
2012-12-15 22:53:22 -0800

    Version 1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# h.js
+
+Escapes HTML entities in the given string.
+
+Returns new string with HTML-specific characters escaped.
+
+## License
+MIT License
---
+++ h.js
@@ -0,0 +1,32 @@
+// Escape HTML entities in the given string
+// Returns new string with HTML-specific characters escaped
+function h(str) {
+  var i, c, out = "", trans, arr;
+  if (typeof (str) === "number" || str === true || str === false) {
+    return "" + str;
+  }
+  else if (!str || !str.length) {
+    return "";
+  }
+  trans = {
+    "<": "&lt;",
+    ">": "&gt;",
+    "\"": "&quot;",
+    "&": "&amp;",
+    "'": "&apos;"
+  };
+
+  arr = str.split("");
+  i = 0;
+  while (i < arr.length) {
+    c = arr[i];
+    if (trans.hasOwnProperty(c)) {
+      out += trans[c];
+    }
+    else {
+      out += c;
+    }
+    i += 1;
+  }
+  return out;
+}
